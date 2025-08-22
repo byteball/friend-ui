8886b9d51b410613202de4bdf984adb5c40168d4
Taump <taump@yandex.ru>
2025-08-22 13:24:29 +0200

    feat(home): replace placeholder with animated friend list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { AnimatedFriendList } from "@/components/layouts/animated-friend-list";
+
 export default function Home() {
   return (
     <div>
-      Hello world!
+      <div className="grid grid-cols-2 *:gap-4">
+        <div>test</div>
+        <AnimatedFriendList />
+      </div>
     </div>
   );
 }
