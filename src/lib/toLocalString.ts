9944dada46b21097c7db198d402c090e39decf42
Taump <taump@yandex.ru>
2025-09-05 14:47:37 +0200

    feat(toLocalString): add locale parameter for formatting

--- src/lib/toLocalString.ts
+++ src/lib/toLocalString.ts
@@ -1,5 +1,5 @@
-export const toLocalString = (numberOrString: string | number) => {
-  return Number(+Number(numberOrString).toFixed(9)).toLocaleString(undefined, {
+export const toLocalString = (numberOrString: string | number, locale: string = 'en-US') => {
+  return Number(+Number(numberOrString).toFixed(9)).toLocaleString(locale, {
     maximumFractionDigits: 18,
     maximumSignificantDigits: 9,
   });
