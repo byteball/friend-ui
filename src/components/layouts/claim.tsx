4af926f6e5fe20b3fe2a40d27ff4114837dce5c6
Taump <taump@yandex.ru>
2025-09-13 20:04:25 +0200

    fix(claim): remove tokens prop from ClaimForm

--- src/components/layouts/claim.tsx
+++ src/components/layouts/claim.tsx
@@ -13,7 +13,7 @@ export const Claim: FC<ClaimProps> = ({ tokens }) => {
   return <div className="gap-4 max-w-4xl mx-auto mt-16">
     <div className="rounded-lg bg-gray-50">
       <div className="px-4 py-5 sm:p-6 w-full">
-        <ClaimForm tokens={tokens ?? []} />
+        <ClaimForm />
       </div>
     </div>
   </div>
