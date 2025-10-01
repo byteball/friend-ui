64a796bfafe7d63dfcd315f789129c198a035483
Taump <taump@yandex.ru>
2025-10-01 16:45:14 +0200

    feat(governance): add nanoid dependency and update usage

--- src/app/governance/components/governance-item-footer.tsx
+++ src/app/governance/components/governance-item-footer.tsx
@@ -6,10 +6,8 @@ interface GovernanceItemFooterProps<K extends keyof AgentParams> {
   name: K;
 }
 
-export const GovernanceItemFooter = <K extends keyof AgentParams>({ name }: GovernanceItemFooterProps<K>) => {
-  return (<CardFooter>
-    <GovernanceModal name={name}>
-      <Button variant="link" className="p-0 m-0">suggest another value</Button>
-    </GovernanceModal>
-  </CardFooter>)
-}
+export const GovernanceItemFooter = <K extends keyof AgentParams>({ name }: GovernanceItemFooterProps<K>) => ((<CardFooter>
+  <GovernanceModal name={name} defaultValue={undefined}>
+    <Button variant="link" className="p-0 m-0">suggest another value</Button>
+  </GovernanceModal>
+</CardFooter>))
