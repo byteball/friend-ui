af14fc6b3f51a1298e3ad5d11c85b7912e6c7806
Taump <taump@yandex.ru>
2025-10-03 18:43:17 +0200

    feat(progress): integrate ProgressProvider in ClientProviders

--- src/app/client-providers.tsx
+++ src/app/client-providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ProgressProvider } from '@bprogress/next/app';
 import { CookiesNextProvider } from "cookies-next";
 import { ReactNode } from "react";
 
@@ -8,5 +9,14 @@ interface ProvidersProps {
 }
 
 export function ClientProviders({ children }: ProvidersProps) {
-  return <CookiesNextProvider pollingOptions={{ enabled: true, intervalMs: 1000 }}>{children}</CookiesNextProvider>;
+  return <ProgressProvider
+    height="4px"
+    color="#1447e6"
+    options={{ showSpinner: false }}
+    shallowRouting
+  >
+    <CookiesNextProvider pollingOptions={{ enabled: true, intervalMs: 1000 }}>
+      {children}
+    </CookiesNextProvider>
+  </ProgressProvider>;
 }
