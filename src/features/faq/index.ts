4a90987a4b97e5ea17cec66e8e872e3412803558
Taump <taump@yandex.ru>
2025-10-05 18:13:24 +0200

    feat(faq): add FaqItem, FaqContainer, FaqContent, and FaqTitle components

--- src/features/faq/index.ts
+++ src/features/faq/index.ts
@@ -1,7 +1,7 @@
-import { FaqItem } from './ui/governance-item';
-import { FaqContainer } from './ui/governance-item-container';
-import { FaqContent } from './ui/governance-item-content';
-import { FaqTitle } from './ui/governance-item-title';
+import { FaqItem } from './ui/faq-item';
+import { FaqContainer } from './ui/faq-item-container';
+import { FaqContent } from './ui/faq-item-content';
+import { FaqTitle } from './ui/faq-item-title';
 
 export default {
   Container: FaqContainer,
