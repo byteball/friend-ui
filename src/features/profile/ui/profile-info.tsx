40f1352eccdc84001bbe5f43cd3f7a53df6795e9
Taump <taump@yandex.ru>
2025-10-13 14:47:30 +0200

    feat(profile-info): add ProfileShareLinks component

--- src/features/profile/ui/profile-info.tsx
+++ src/features/profile/ui/profile-info.tsx
@@ -6,11 +6,14 @@ import { BOUNCE_FEES } from "@/constants";
 import { generateLink } from "@/lib/generateLink";
 
 import { ActiveUserLabel } from "./active-user-label";
+import { ProfileShareLinks } from "./profile-share-links";
 
 import { getProfileUsername } from "@/lib/getProfileUsername.server";
 import { isActiveUser } from "@/lib/is-active-user";
 
 import { appConfig } from "@/appConfig";
+import { env } from "@/env";
+
 interface ProfileInfoProps {
   address: string;
   userData?: IUserData;
@@ -32,18 +35,27 @@ export const ProfileInfo: FC<ProfileInfoProps> = async ({
     }
   });
 
-  return (<div className="flex items-center justify-between">
-    <div className="flex space-x-4">
-      <h1 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-6xl first-letter:uppercase">
-        {username}&apos;s profile
-      </h1>
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex space-x-4">
+          <h1 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-6xl first-letter:uppercase">
+            {username}&apos;s profile
+          </h1>
 
-      <ActiveUserLabel isActive={isActive} />
-    </div>
+          <ActiveUserLabel isActive={isActive} />
+        </div>
 
-    <div className="flex flex-col items-end gap-2">
-      <QRButton href={connectUrl} disabled={!isActive} variant="secondary">Add friend</QRButton>
-      <small className="text-xs text-muted-foreground">Before sending a request, please contact {username} first</small>
-    </div>
-  </div>)
+        <div className="flex flex-col items-end gap-2">
+          <QRButton href={connectUrl} disabled={!isActive} variant="secondary">Add friend</QRButton>
+          <small className="text-xs text-muted-foreground">Before sending a request, please contact {username} first</small>
+        </div>
+      </div>
+
+      <ProfileShareLinks
+        url={`${env.NEXT_PUBLIC_SITE_URL}/user/${address}`}
+        title={`Become friends with ${username} on Obyte friend!`}
+      />
+    </>
+  )
 }
