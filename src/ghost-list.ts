32a8e5ffadb510378757b2b25a5304bed3ab91db
Taump <taump@yandex.ru>
2025-10-17 15:09:36 +0200

    feat(ghosts): add initial ghost list with images

---
+++ src/ghost-list.ts
@@ -0,0 +1,32 @@
+export const ghostList = {
+  "Tim May": {
+    name: "Tim May",
+    image: "/ghosts/tim-may.png",
+  },
+  "Hal Finney": {
+    name: "Hal Finney",
+    image: "/ghosts/hal-finney.png",
+  },
+  "Phil Zimmermann": {
+    name: "Phil Zimmermann",
+    image: "/ghosts/phil-zimmermann.png",
+  },
+  "Nick Szabo": {
+    name: "Nick Szabo",
+    image: "/ghosts/nick-szabo.jpg",
+  },
+  "Adam Back": {
+    name: "Adam Back",
+    image: "/ghosts/adam-back.png",
+  },
+  "David Friedman": {
+    name: "David Friedman",
+    image: "/ghosts/david-friedman.png",
+  },
+  "Wei Dai": {
+    name: "Wei Dai",
+  },
+  "Satoshi Nakamoto": {
+    name: "Satoshi Nakamoto"
+  },
+}
