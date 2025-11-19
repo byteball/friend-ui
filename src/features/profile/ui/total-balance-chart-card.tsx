169806a1098fa5c97530db464b11fa4dee4de5e5
Taump <taump@yandex.ru>
2025-11-19 13:43:38 +0100

    refactor(total-balance-chart-card): improve AreaChart accessibility and adjust XAxis tick gap

--- src/features/profile/ui/total-balance-chart-card.tsx
+++ src/features/profile/ui/total-balance-chart-card.tsx
@@ -80,20 +80,23 @@ export function TotalBalanceChartCardProps({ address }: TotalBalanceChartCardPro
             config={totalBalanceChartConfig}
             className="aspect-auto h-[260px] w-full"
           >
-            <AreaChart data={normalizedBalanceSeries}>
-              <defs>
+            <AreaChart
+              accessibilityLayer
+              data={normalizedBalanceSeries}
+            >
+              {/* <defs>
                 <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="#1447e5" stopOpacity={0.8} />
                   <stop offset="95%" stopColor="#1447e5" stopOpacity={0.1} />
                 </linearGradient>
-              </defs>
+              </defs> */}
 
               <XAxis
                 dataKey="trigger_date"
                 tickLine={false}
                 axisLine={false}
                 tickMargin={8}
-                minTickGap={1}
+                minTickGap={18}
                 tickFormatter={(value) => {
                   const date = new Date(value)
                   return date.toLocaleDateString("en-US", {
