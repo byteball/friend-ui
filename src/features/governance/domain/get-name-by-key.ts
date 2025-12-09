098a04da07f7d2e7d211a513487a431dc51d56d6
Taump <taump@yandex.ru>
2025-12-09 18:37:02 +0100

    refactor(get-name-by-key): enhance key formatting logic

--- src/features/governance/domain/get-name-by-key.ts
+++ src/features/governance/domain/get-name-by-key.ts
@@ -1,11 +1,23 @@
 export const getNameByKey = (key: keyof AgentParams) => {
-  return key
+  let newKey = key
     .slice(0, 1)
     .toUpperCase()
 
     + key.slice(1)
       .split("_")
       .join(" ")
-      .replaceAll("aa", "AA")
-      .replaceAll("frd", "FRD");
+      .replaceAll("aa", "AA");
+
+
+  if (newKey.endsWith("deposit reward share")) {
+    if (newKey.includes("bytes")) {
+      newKey = "Referrer deposit reward share for GBYTE";
+    } else if (newKey.includes("frd")) {
+      newKey = "Referrer deposit reward share for FRD";
+    } else if (newKey.includes("deposit asset")) {
+      newKey = "Referrer deposit reward share for external deposit assets";
+    }
+  }
+
+  return newKey;
 }
