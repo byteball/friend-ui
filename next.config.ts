01d19a67e53d1e176583a68f878b7a824d91f9e5
Taump <taump@yandex.ru>
2025-12-10 14:41:44 +0100

    chore(deps): upgrade Next.js and React dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["obyte"],
-    reactCompiler: {
-      compilationMode: "all"
-    }
   },
+  reactCompiler: true,
   output: "standalone",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   async rewrites() {
     return [
       {
@@ -24,17 +17,7 @@ const nextConfig: NextConfig = {
         destination: '/:id?r=p',
       },
     ];
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push('bufferutil', 'utf-8-validate');
-    } else {
-      config.resolve.alias['bufferutil'] = false;
-      config.resolve.alias['utf-8-validate'] = false;
-    }
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
