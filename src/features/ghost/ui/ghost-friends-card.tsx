00f482fe9740dfb98be29fcfc57b6583069ab5ff
Taump <taump@yandex.ru>
2025-12-24 11:18:11 +0100

    style(ghost-friends-card): improve layout and styling

--- src/features/ghost/ui/ghost-friends-card.tsx
+++ src/features/ghost/ui/ghost-friends-card.tsx
@@ -52,15 +52,20 @@ export const GhostFriendsCard: FC<IGhostFriendsProps> = ({ userData, address }) 
         <CardTitle className="min-h-12">
           {isLoading
             ? <Skeleton className="w-[calc(100%-20px)] h-12" />
-            : <div className="leading-6 max-w-lg">On {userData?.current_streak || 0} out of {requiredStreak} days streak to become friends with the ghost of {ghostName}</div>}
+            : <div className="max-w-lg leading-6">On {userData?.current_streak || 0} out of {requiredStreak} days streak to become friends with the ghost of {ghostName}</div>}
         </CardTitle>
       </CardHeader>
-      <CardContent className={cn({ "min-h-[270px]": address === walletAddress, "min-h-60": address !== walletAddress })}>
-        <div className="flex lg:flex-row flex-col justify-between gap-4">
-          <div className="mt-2 flex flex-col">
+      <CardContent
+        className={cn({
+          "min-h-[270px]": address === walletAddress,
+          "min-h-60": address !== walletAddress
+        })}
+      >
+        <div className="flex flex-col justify-between gap-4 lg:flex-row">
+          <div className="flex flex-col mt-2">
 
             <TooltipProvider>
-              <div className="*:data-[slot=tooltip-trigger]:ring-background flex -space-x-2 *:data-[slot=tooltip-trigger]:ring-2 *:data-[slot=tooltip-trigger]:hover:z-10">
+              <div className="*:data-[slot=tooltip-trigger]:ring-[#60A5FA] flex -space-x-2 *:data-[slot=tooltip-trigger]:ring-2 *:data-[slot=tooltip-trigger]:hover:z-10">
 
                 {!isLoading ? allGhosts.map((g, index) => <Tooltip key={g.name}>
                   <TooltipTrigger asChild>
@@ -73,17 +78,17 @@ export const GhostFriendsCard: FC<IGhostFriendsProps> = ({ userData, address }) 
                     <p>{g.name}</p>
                   </TooltipContent>
                 </Tooltip>) : Array.from({ length: 5 }).map((_, index) => (
-                  <div key={index} className="rounded-full overflow-hidden border-2 border-background ring-background ring-2">
-                    <Skeleton className="w-10 h-10 rounded-full" />
+                  <div key={index} className="overflow-hidden border-2 rounded-full ring-[#60A5FA] ring-2">
+                    <Skeleton className="w-8 h-8 rounded-full" />
                   </div>
                 ))}
               </div>
             </TooltipProvider>
 
-            {isLoading ? <div className="grid mt-4 gap-2">
+            {isLoading ? <div className="grid gap-2 mt-4">
               <Skeleton className="w-full h-4" />
               <Skeleton className="w-full h-4" />
-            </div> : <div className="mt-4 gap-2">
+            </div> : <div className="gap-2 mt-4">
               <div>Current streak: {userData?.current_streak || 0} / {requiredStreak} days</div>
               <div>Total streak: {userData?.total_streak || 0} day(s)</div>
             </div>}
@@ -109,15 +114,15 @@ export const GhostFriendsCard: FC<IGhostFriendsProps> = ({ userData, address }) 
           </div>
 
           {isLoading
-            ? <div className="w-full lg:w-auto flex justify-center lg:justify-end">
+            ? <div className="flex justify-center w-full lg:w-auto lg:justify-end">
               <Skeleton className="w-full max-w-[230px] aspect-square" />
               {address === walletAddress ? <Skeleton className="w-full h-9" /> : null}
             </div>
-            : <div className="text-center w-full lg:w-auto flex flex-col items-center lg:items-end">
+            : <div className="flex flex-col items-center w-full text-center lg:w-auto lg:items-end">
               <div className="border p-2 w-full max-w-[230px] aspect-square relative bg-accent rounded-md overflow-hidden animate-pulse [&:has(img)]:animate-none">
                 <Image
                   wrapperClassName="w-full h-full"
-                  className="w-full h-full object-cover"
+                  className="object-cover w-full h-full"
                   src={`/api/puzzle/${address}?t=${Math.floor(Date.now() / (60 * 1000))}`} // cache buster every minute
                   fallback="/ghosts/default.png"
                   loading="eager"
