aa106d2084eeadeac8fc7531491994642ac40ae5
Taump <taump@yandex.ru>
2026-01-02 14:40:25 +0100

    fix(is-active-user): include isSameDay check for unlock date

--- src/lib/is-active-user.ts
+++ src/lib/is-active-user.ts
@@ -1,5 +1,5 @@
 import { appConfig } from "@/app-config";
-import { addDays, isAfter, parseISO } from "date-fns";
+import { addDays, isAfter, isSameDay, parseISO } from "date-fns";
 
 export const isActiveUser = (userData?: IUserData | undefined): boolean => {
   if (!userData) return false;
@@ -7,5 +7,5 @@ export const isActiveUser = (userData?: IUserData | undefined): boolean => {
   const unlockDate = parseISO(userData.unlock_date);
 
   const minLockedDate = unlockDate ? addDays(new Date(), appConfig.MIN_LOCKED_TERM_DAYS) : null;
-  return minLockedDate && unlockDate ? isAfter(unlockDate, minLockedDate) : false;
+  return minLockedDate && unlockDate ? isAfter(unlockDate, minLockedDate) || isSameDay(unlockDate, minLockedDate) : false;
 }
