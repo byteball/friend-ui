5855462b89b47ead9005289ebbfa1aaaaa7281ca
Taump <taump@yandex.ru>
2026-01-04 12:06:14 +0100

    fix(get-current-streak): use userData.last_date for lastDate

--- src/lib/get-current-streak.ts
+++ src/lib/get-current-streak.ts
@@ -4,7 +4,7 @@ export const getCurrentStreak = (userData: IUserData | undefined) => {
   if (!userData || !userData.last_date || !userData.current_streak) return 0;
 
   const today = new Date().setUTCHours(0, 0, 0, 0);
-  const lastDate = new Date(`2026-01-03T00:00:00Z`).getTime();
+  const lastDate = new Date(`${userData.last_date}T00:00:00Z`).getTime();
   const yesterday = subDays(today, 1).getTime();
 
   if (lastDate === yesterday || lastDate === today) {
