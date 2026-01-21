732940ad10e78a3a1953c6e3c941bea240683e2e
Taump <taump@yandex.ru>
2026-01-21 16:37:47 +0100

    feat(governance): set default value for balance parameter

--- src/lib/calculations/get-vp-by-balance.ts
+++ src/lib/calculations/get-vp-by-balance.ts
@@ -1,4 +1,4 @@
-export const getVPByBalance = (balance: Balances, ceilingPrice: number = 1, decimals: number = 9): number => {
+export const getVPByBalance = (balance: Balances = {}, ceilingPrice: number = 1, decimals: number = 9): number => {
   const balanceInFrd = (balance.frd ?? 0) + ((balance.base ?? 0) / ceilingPrice);
 
   return Math.sqrt(balanceInFrd / 10 ** decimals);
